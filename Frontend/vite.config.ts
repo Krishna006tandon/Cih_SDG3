7f84b692dd3736d425cfc8814a67e01e824e18b3
Krishna006tandon <krishnatandon006@gmail.com>
2026-01-30 22:02:41 +0530

    deployement fix

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -18,7 +21,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
